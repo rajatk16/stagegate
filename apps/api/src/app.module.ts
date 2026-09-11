8c5dc4563e4cb02eb5157c8cb6f2a048f1aac8d1
sudagar <rajat.sudagade@autodesk.com>
2026-09-11 12:20:28 +0530

    Apply Prettier formatting to API app module import

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { FirebaseAdminModule, RuntimeConfigModule } from '@stagegate/backend-platform';
+import {
+  FirebaseAdminModule,
+  RuntimeConfigModule,
+} from '@stagegate/backend-platform';
 
 import { AuthModule } from './platform/auth';
 import { TenancyModule } from './platform/tenancy';
